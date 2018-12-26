efeb90c979ecaddc71edb7b257b0723a94c04652
royswale <royswale@gmail.com>
2018-12-26 14:08:58 +0800

    finish quicksort

--- aha-algorithms/01.sort/03.quick-sort.go
+++ aha-algorithms/01.sort/03.quick-sort.go
@@ -2,46 +2,76 @@ package main
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 	"math/rand"
 	"time"
 )
 
+const N  = 100
+const M  = 10
 
 var d = make([]int, 0)
 
 func quickSort(left int, right int) {
 
-	base := d[left]
-	i := left
-	j := right
-
 	if left >= right {
 		return
 	}
 
-	for left < right  {
-		//if left === right {
-		//	return
-		//}
+	base := d[left]
+	i := left
+	j := right
 
 
-		for d[right] >= base && left < right {
-			right--
+	for i < j  {
+
+		for d[j] >= base && i < j {
+			j--
 		}
 
-		for d[left] <= base && left < right {
-			left++
+		for d[i] <= base && i < j {
+			i++
 		}
 
-		if left < right {
-			d[left], d[right] = d[right], d[left]
+		if i < j {
+			d[i], d[j] = d[j], d[i]
+			highlight(i, j)
 		}
 
 	}
 
+	d[i], d[left] = d[left], d[i]
+	highlightSwitchBase(left, i)
 
 	quickSort(left, i-1)
 	quickSort(i+1, right)
+}
+
+func highlight(i, j int)  {
+	red := color.New(color.FgWhite, color.Bold, color.BgGreen)
+	//fmt.Println()
+	for index, v := range d {
+		if index == i || index == j {
+			red.Printf("%d", v)
+			fmt.Printf(" ")
+		} else {
+			fmt.Printf("%d ", v)
+		}
+	}
+	fmt.Println()
+}
+func highlightSwitchBase(i, j int)  {
+	red := color.New(color.FgWhite, color.Bold, color.BgYellow)
+	//fmt.Println()
+	for index, v := range d {
+		if index == i || index == j {
+			red.Printf("%d", v)
+			fmt.Printf(" ")
+		} else {
+			fmt.Printf("%d ", v)
+		}
+	}
+	fmt.Println()
 }
 
 func main()  {
@@ -49,11 +79,13 @@ func main()  {
 	newRand := rand.New(newSeeder)
 
 	//d := make([]int, 0)
-	for i := 0; i < 20; i++ {
-		d = append(d, newRand.Intn(101))
+	for i := 0; i < M; i++ {
+		d = append(d, newRand.Intn(N + 1))
 	}
 
-	quickSort(0, 20)
+	fmt.Println(d)
+
+	quickSort(0, M - 1)
 
 	fmt.Println(d)
 }
